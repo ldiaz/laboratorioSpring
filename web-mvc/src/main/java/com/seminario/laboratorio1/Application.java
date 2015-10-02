4989fccc268d10c518253797ac7500a52d25fa4a
Leonardo Diaz <ingleonardo@gmail.com>
2015-10-02 17:46:51 -0500

    Proyecto base de spring boot con spring-mvc

---
+++ web-mvc/src/main/java/com/seminario/laboratorio1/Application.java
@@ -0,0 +1,12 @@
+package com.seminario.laboratorio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Application {
+
+    public static void main(String[] args) {
+       // SpringApplication.run(Application.class, args);
+    }
+
+}
