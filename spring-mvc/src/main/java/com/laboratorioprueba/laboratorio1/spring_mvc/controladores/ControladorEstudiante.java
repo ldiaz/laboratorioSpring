07db2cb3fa164a0dc5da9f9b4df2671ccd4c0a51
Leonardo Diaz <ingleonardo@gmail.com>
2015-10-09 16:11:41 -0500

    Formulario actualizado usando thymeleaf para describir objeto tipo estudiante dentro del Model y controlador actualizado para enviar objeto estudiante en cada metodo a la interfaz

--- spring-mvc/src/main/java/com/laboratorioprueba/laboratorio1/spring_mvc/controladores/ControladorEstudiante.java
+++ spring-mvc/src/main/java/com/laboratorioprueba/laboratorio1/spring_mvc/controladores/ControladorEstudiante.java
@@ -1,8 +1,12 @@
 package com.laboratorioprueba.laboratorio1.spring_mvc.controladores;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.laboratorioprueba.laboratorio1.spring_mvc.domain.Estudiante;
+
 
 import org.springframework.stereotype.Controller;
 
@@ -12,7 +16,17 @@ public class ControladorEstudiante {
 	@RequestMapping(value="estudiante", method=RequestMethod.GET)
 	public String formularioEstudiante(Model modelo){
 		
+		System.out.println("Obteniendo el formulario");
+		modelo.addAttribute("estudiante", new Estudiante());
+		
 		return "formularioEstudiante";
 	}
 	
+	@RequestMapping(value="estudiante", method=RequestMethod.POST)
+	public String crearEstudiante(@ModelAttribute Estudiante estudiante, Model modelo){
+		
+		System.out.println("posteando desde el formulario");
+
+		return "formularioEstudiante";
+	}
 }
